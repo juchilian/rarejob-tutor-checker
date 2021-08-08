e1cc825ffef8a4db24a790ab533e22291a604f51
juchilian <juchilian1227@gmail.com>
2021-08-08 12:15:36 +0900

    [bug] temp save

--- server/index.py
+++ server/index.py
@@ -1,11 +1,19 @@
-class Sample():
-    def __init__(self, a, b) -> None:
-        self.a = a
-        self.b = b
-    
-    def addition(self):
-        return self.a + self.b
+import os
 
-print('Executed: index.py')
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Remote(
+    command_executor=os.environ["SELENIUM_URL"],
+    desired_capabilities=DesiredCapabilities.CHROME.copy()
+)
+driver.implicitly_wait(5)
+
+driver.get("https://www.rarejob.com/account/login/")
+
+print(driver.title)
+driver.quit()
     
 
