86325dbde71f17235363621748e9645e09882576
juchilian <juchilian1227@gmail.com>
2021-08-09 08:42:33 +0900

    save prev

--- server/index.py
+++ server/index.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
-    desired_capabilities=DesiredCapabilities.CHROME.copy()
+    desired_capabilities=DesiredCapabilities.CHROME
 )
 driver.implicitly_wait(5)
 
