c4433d5e361d9a62eb0b04eade49f98617e6e857
juchilian <juchilian1227@gmail.com>
2021-08-09 09:04:40 +0900

    [bug] new version

--- server/index.py
+++ server/index.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
+    # command_executor="http://localhost:4444/wd/hub",
     desired_capabilities=DesiredCapabilities.CHROME
 )
 driver.implicitly_wait(5)
